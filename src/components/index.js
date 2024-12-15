4f81065710286e890d96955ff161f37f8d972dd3
Zain Raza <82883448+Zain151214@users.noreply.github.com>
2024-12-15 21:57:38 +0500

    fix import issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Button from './global/Button';
-import Navbar from './navigation/navbar';
+import Navbar from './navigation/Navbar';
 import Loader from './global/Loader';
 
 export { Button, Navbar, Loader };
